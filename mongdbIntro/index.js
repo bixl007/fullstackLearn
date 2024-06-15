5c3e04db38a2ef78f2016b2e3d5bbac7daeb5d40
Bishal Baira <bishalbaira432@gmail.com>
2024-06-15 21:35:09 +0530

    Feat: MongoDB introduction file added

---
+++ mongdbIntro/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://Bishal:Vm52210@cluster0.zt5fkkp.mongodb.net/userappnew"
+);
+
+const User = mongoose.model("Users", {
+  name: String,
+  email: String,
+  password: String,
+});
+
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+  const existingUser = await User.findOne({ email: username });
+  if (existingUser) {
+    return res.status(400).send("Username already exists");
+  }
+
+  const user = new User({
+    name: name,
+    email: username,
+    password: password,
+  });
+
+  user.save();
+  res.json({ message: "User created successfully" });
+});
