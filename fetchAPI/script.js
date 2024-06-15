bbe6c9a05ca688b7a6942bbcc42366799fee685d
Bishal Baira <bishalbaira432@gmail.com>
2024-06-15 09:25:52 +0530

    Feat: Fetch API files modified: added display feature

--- fetchAPI/script.js
+++ fetchAPI/script.js
@@ -8,12 +8,17 @@
 //     })
 // }
 
+async function getAnimalData() {
+  const response = await fetch("https://fakerapi.it/api/v1/persons");
+  const finalResult = await response.json();
+  console.log(finalResult);
+}
 
-
-
-
-async function getAnimalData(){
-    const response = await fetch("https://fakerapi.it/api/v1/persons");
-    const finalResult = await response.json()
-    console.log(finalResult);
+async function getUserData() {
+  const response = await fetch("https://fakerapi.it/api/v1/persons");
+  const finalResult = await response.json();
+//   console.log(finalResult);
+  document.getElementById("dataDisplay").innerHTML =
+    JSON.stringify(finalResult);
+    console.log("Data Display!!!")
 }
