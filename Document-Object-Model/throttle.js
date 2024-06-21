7ef6d7ac4f68e286d84add695e24f8fa7d276257
Bishal Baira <bishalbaira432@gmail.com>
2024-06-21 16:23:59 +0530

    feat: Debouncind and throttle handling added

---
+++ Document-Object-Model/throttle.js
@@ -0,0 +1,17 @@
+let timeout;
+function debounce(){
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+        populateDiv()
+    },300)
+}
+
+
+async function populateDiv(){
+    const a = document.getElementById("first").value;
+    const b = document.getElementById("second").value;
+
+    const res = await fetch(`https://sum-server.100xdevs.com/sum?a=${a}&b=${b}`);
+    const ans = await res.text();
+    document.getElementById("finalSum").innerHTML=`The sum of the elements is ${ans}`;
+}
