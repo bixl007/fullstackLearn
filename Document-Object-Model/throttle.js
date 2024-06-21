5093714e7fda2d0811bc96020669f18dd140c9a4
Bishal Baira <bishalbaira432@gmail.com>
2024-06-21 16:24:54 +0530

    feat: Debouncing and throttle handling added

--- Document-Object-Model/throttle.js
+++ Document-Object-Model/throttle.js
@@ -1,17 +1,18 @@
 let timeout;
-function debounce(){
-    clearTimeout(timeout);
-    timeout = setTimeout(function(){
-        populateDiv()
-    },300)
+function debounce() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    populateDiv();
+  }, 300);
 }
 
+async function populateDiv() {
+  const a = document.getElementById("first").value;
+  const b = document.getElementById("second").value;
 
-async function populateDiv(){
-    const a = document.getElementById("first").value;
-    const b = document.getElementById("second").value;
-
-    const res = await fetch(`https://sum-server.100xdevs.com/sum?a=${a}&b=${b}`);
-    const ans = await res.text();
-    document.getElementById("finalSum").innerHTML=`The sum of the elements is ${ans}`;
+  const res = await fetch(`https://sum-server.100xdevs.com/sum?a=${a}&b=${b}`);
+  const ans = await res.text();
+  document.getElementById(
+    "finalSum"
+  ).innerHTML = `The sum of the elements is ${ans}`;
 }
