06bffb73eaa4b3bc2b1ac24b6f0095cac35ccbb7
Bishal Baira <bishalbaira432@gmail.com>
2024-07-01 16:59:50 +0530

    Feat: useMemo added

---
+++ react-useMemo/src/App.jsx
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = useMemo(() => {
+    let finalCount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalCount = finalCount + i;
+    }
+    return finalCount;
+  }, [inputValue]);
+
+  return (
+    <div>
+      <input
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder={"Find sum from 1 to n"}
+      ></input>
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter ({counter})
+      </button>
+    </div>
+  );
+}
+
+export default App;
