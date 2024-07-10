9d6227a12b8c1b7911fda307f743f299882bf8d0
Bishal Baira <bishalbaira432@gmail.com>
2024-07-10 15:38:32 +0530

    Feat: typescript prac updated

--- 26_typescript/src/04_practice.ts
+++ 26_typescript/src/04_practice.ts
@@ -8,10 +8,34 @@ const ans: boolean = isLegal(20);
 console.log("ans: ", ans);
 */
 
-function runAfter1S(fn: () => void) {
-  setTimeout(fn, 1000);
+// function runAfter1S(fn: () => void) {
+//   setTimeout(fn, 1000);
+// }
+
+// runAfter1S(function () {
+//   console.log("Hello World!");
+// });
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email?: string; // adding ? means it is a optional field
 }
 
-runAfter1S(function () {
-  console.log("Hello World!");
+function isLegal(user: User) {
+  if (user.age > 18) return true;
+  else return false;
+}
+
+function greetUser(user: User) {
+  console.log("Hello " + user.firstName);
+}
+
+const ans = isLegal({
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
 });
+
+console.log(ans);
