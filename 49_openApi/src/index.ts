2519050781c11fdd0b77b6e0dcf47f3203b3c2b4
Bishal Baira <bishalbaira432@gmail.com>
2024-08-20 16:21:10 +0530

    openapi using express

---
+++ 49_openApi/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { openapiSpec } from "./openapispec";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+];
+
+app.get("/users", (req, res) => {
+  const { name } = req.query;
+
+  if (name) {
+      const filteredUsers = users.filter((user) =>
+    //@ts-ignore
+      user.name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(filteredUsers);
+  } else {
+    res.json(users);
+  }
+});
+
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(openapiSpec));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
