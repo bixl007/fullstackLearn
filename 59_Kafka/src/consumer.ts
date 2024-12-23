b978c82c2ce845bc46ab19eb912fbf68406ec582
Bishal Baira <bishalbaira432@gmail.com>
2024-12-23 22:54:00 +0530

    Kafka Jargon

---
+++ 59_Kafka/src/consumer.ts
@@ -0,0 +1,27 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "my-app3" });
+
+async function main() {
+  await consumer.connect();
+  await consumer.subscribe({
+    topic: "quickstart-events",
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        offset: message.offset,
+        value: message?.value?.toString(),
+      });
+    },
+  });
+}
+
+main();
---
+++ 59_Kafka/src/producer.ts
@@ -0,0 +1,22 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+async function main() {
+  await producer.connect();
+  await producer.send({
+    topic: "quickstart-events",
+    messages: [
+      {
+        value: "hi there",
+      },
+    ],
+  });
+}
+
+main();
